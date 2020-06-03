2221b867fbe4497e32468ce9427f06c9ca088aca
Kushail <kushailsingh98@gmail.com>
2020-06-03 15:24:12 +0200

    check

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const mainRouter = require('./mainRoutes')
+const port = process.env.PORT || 3000
+
+app.use(mainRouter)
+app.listen(port)
+console.log('Express server running on port', port)
