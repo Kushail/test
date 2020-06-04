dbd4d2767bfb9c810e226f1db7d690b79b0ccb35
Kushail <kushailsingh98@gmail.com>
2020-06-04 16:38:30 +0200

    working expenses api

---
+++ Expenses.js
@@ -0,0 +1,19 @@
+const expenses = [
+  {
+    id: 1,
+    name: 'Groceries',
+    cost: 100
+  },
+  {
+    id: 2,
+    name: 'Toilet paper',
+    cost: 50
+  },
+  {
+    id: 3,
+    name: 'Soap',
+    cost: 50
+  }
+]
+
+module.exports = expenses
